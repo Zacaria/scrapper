298cf1fb7b9d5c07b72e5c8713654b2e51ea8670
Zacaria <zc@42.am>
2017-02-04 18:39:23 +0100

    [FIX] : remove commnts and useless code

--- src/scrapper/index.js
+++ src/scrapper/index.js
@@ -1,17 +1,5 @@
 import cheerio from 'cheerio';
-import request from 'request';
-import jsdom from 'jsdom';
 import req from 'cheerio-req';
-
-import path from 'path';
-import childProcess from 'child_process';
-import phantomjs from 'phantomjs';
-
-const binPath = phantomjs.path;
-
-const childArgs = [
-  path.join(__dirname, 'phantom.js')
-];
 
 const scrapeHTML = ($, opts) => {
   if (typeof $ === "string") {
@@ -24,7 +12,6 @@ const scrapeHTML = ($, opts) => {
       target: opt.target,
       [opt.target]: $(opt.selector)
         .map(function () {
-          console.log('len', item.innerText);
           return $(this).text();
         })
         .get()
@@ -34,31 +21,6 @@ const scrapeHTML = ($, opts) => {
 
 const requestWebsite = (url, opts) =>
   new Promise((resolve, reject) => {
-    console.log('url', url);
-    // jsdom.env(
-    //   url,
-    //   ['https://code.jquery.com/jquery-3.1.1.min.js'],
-    //   (err, window) => {
-    //     if (err) {
-    //       console.log('err', err);
-    //       return reject(err);
-    //     }
-    //
-    //     // childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-    //     //   if(err) console.log(stderr);
-    //     //   console.log(stdout);
-    //     // });
-    //
-    //     // console.log('body', window.document.querySelectorAll('.title a'));
-    //
-    //     const data = scrapeHTML(window.$, opts);
-    //     console.log('data', opts, data);
-    //     resolve({
-    //       url,
-    //       data
-    //     });
-    //   }
-    // );
     req(url, (err, $, res, body) => {
       if (err) {
         console.log('err', err);
@@ -67,8 +29,7 @@ const requestWebsite = (url, opts) =>
 
       console.log($.html());
 
-      const data = scrapeHTML($, opts);
-      // console.log('data', opts, data)
+      const data = scrapeHTML($, opts);)
       resolve({
         url,
         data
